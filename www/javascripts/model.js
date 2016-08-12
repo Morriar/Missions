49a037a9ed10839913f624fdcd8a8e0865472150
Alexandre Terrasa <alexandre@moz-code.org>
2016-08-12 13:40:14 -0400

    ng: introduce model
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

---
+++ www/javascripts/model.js
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2016 Alexandre Terrasa <alexandre@moz-code.org>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+(function() {
+	var apiUrl = '/api';
+
+	angular
+		.module('model', [])
+
+		.factory('Players', [ '$http', function($http) {
+			return {
+				getPlayers: function(cb, cbErr) {
+					$http.get(apiUrl + '/players')
+						.success(cb)
+						.error(cbErr);
+				},
+				getPlayer: function(login, cb, cbErr) {
+					$http.get(apiUrl + '/players/' + login)
+						.success(cb)
+						.error(cbErr);
+				},
+				getAuth: function(cb, cbErr) {
+					$http.get(apiUrl + '/player')
+						.success(cb)
+						.error(cbErr);
+				},
+				getTracksStatus: function(login, cb, cbErr) {
+					$http.get(apiUrl + '/players/' + login + '/tracks')
+						.success(cb)
+						.error(cbErr);
+				},
+				getTrackStatus: function(login, trackId, cb, cbErr) {
+					$http.get(apiUrl + '/players/' + login + '/tracks/' + trackId)
+						.success(cb)
+						.error(cbErr);
+				},
+				getMission: function(login, missionId, cb, cbErr) {
+					$http.get(apiUrl + '/missions/' + missionId)
+						.success(cb)
+						.error(cbErr);
+				},
+				getMissionStatus: function(login, missionId, cb, cbErr) {
+					$http.get(apiUrl + '/players/' + login + '/missions/' + missionId)
+						.success(cb)
+						.error(cbErr);
+				}
+			}
+		}])
+})();
