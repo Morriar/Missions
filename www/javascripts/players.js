76d783ea16c846806c3358e82f3281a86b47bc1d
Alexandre Terrasa <alexandre@moz-code.org>
2016-08-12 13:49:59 -0400

    ng: add track graph using dagre d3
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- www/javascripts/players.js
+++ www/javascripts/players.js
@@ -105,4 +105,87 @@
 				controllerAs: 'playerCtrl'
 			};
 		}])
+
+		.directive('trackTree', [function() {
+			return {
+				replace: true,
+				restrict: 'E',
+				scope: {
+					missionsStatus: '='
+				},
+				controller: 'PlayerCtrl',
+				controllerAs: 'playerCtrl',
+				templateUrl: '/directives/track-tree.html',
+				link: function ($scope, element, attrs) {
+					$scope.buildMap = function(missionsStatus) {
+						var map = {};
+						missionsStatus.forEach(function(status) {
+							map[status.mission._id] = status;
+						});
+						return map;
+					}
+					$scope.drawTree = function(missionsStatus) {
+						var svg = d3.select(element[0])
+						var inner = svg.select("g")
+						var render = new dagreD3.render();
+
+						// Left-to-right layout
+						var g = new dagreD3.graphlib.Graph()
+							.setGraph({
+								nodesep: 20,
+								ranksep: 40,
+								rankdir: "LR",
+								marginx: 10,
+								marginy: 10
+							})
+						  .setDefaultEdgeLabel(function() { return {}; });
+
+						function draw(isUpdate) {
+							var map = $scope.buildMap(missionsStatus);
+							missionsStatus.forEach(function(status) {
+								g.setNode(status.mission._id, {
+									labelType: "html",
+									label: "<div class=''>" + status.mission.title + "</div>",
+									rx: 5,
+									ry: 5,
+									padding: 0,
+									id: status.mission._id,
+									class: status.status
+								});
+								status.mission.parents.__items.forEach(function(parent) {
+									g.setEdge(parent, status.mission._id, {
+										width: 40,
+										class: map[parent].status
+									});
+								});
+							});
+
+							render(inner, g);
+
+							// Zoom and scale to fit
+							var graphWidth = g.graph().width;
+							var graphHeight = g.graph().height;
+							var width = parseInt(svg.style("width").replace(/px/, ""));
+							var height = parseInt(svg.style("height").replace(/px/, ""));
+							var zoomScale = Math.min(width / graphWidth, height / graphHeight);
+							var translate = [(width/2) - ((graphWidth*zoomScale)/2), (height/2) - ((graphHeight*zoomScale)/2)];
+
+							var zoom = d3.behavior.zoom().on("zoom", function() {
+								inner.attr("transform", "translate(" + d3.event.translate + ")" +
+									"scale(" + d3.event.scale + ")");
+							});
+							zoom.translate(translate);
+							zoom.scale(zoomScale);
+							zoom.event(isUpdate ? svg.transition().duration(500) : d3.select("svg"));
+						}
+						draw();
+					}
+
+					$scope.$watch('missionsStatus', function(missionsStatus) {
+						if(!missionsStatus) { return; }
+						$scope.drawTree(missionsStatus);
+					})
+				}
+			};
+		}])
 })();
